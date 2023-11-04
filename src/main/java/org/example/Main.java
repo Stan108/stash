70c3c0a9cf1f865bfa435148679e0a39b6a8120d
Stan108 <kage74@yandex.ru>
2023-11-04 17:26:40 +0300

    conflict commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,9 @@ public class Main {
         System.out.println("Om Shri Sai Ram!");
         System.out.println("Om Shri Sai Ram!");
         System.out.println("Om Shri Sai Ram!");
-        System.out.println("Hello git!!!");
-        System.out.println("Hello git!!!");
-        System.out.println("Hello git!!!");
-        System.out.println("Hello git!!!");
+        System.out.println("Hel");
+        System.out.println("He");
+        System.out.println("He");
         System.out.println("Hello git!!!");
         System.out.println("Hello git!!!");
         IntStream.range(0, 10).mapToObj(i -> "Om!!!").forEach(System.out::println);
