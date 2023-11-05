7deb702c0db6d349bbb195bc70a1d67bcb4ece29
Stan108 <kage74@yandex.ru>
2023-11-05 17:50:49 +0300

    4 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Om Shri Sai Ram!");
         System.out.println("Om Shri Sai Ram!");
         System.out.println("Om Shri Sai Ram!");
+        System.out.println("Om Shri Sai Ram!");
     }
 }
